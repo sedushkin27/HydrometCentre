85ab130e4169457ac861f0480a353ffad1306b24
Vadim <sedushkin27@gmail.com>
2023-08-14 11:35:31 +0300

    Добавьте файлы проекта.

---
+++ HomeworkHydrometCentre/HydrometCentre.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkHydrometCentre
+{
+    internal class HydrometCentre
+    {
+        TemperatureInformation[] temperatureInformation = new TemperatureInformation[30];
+
+        public void AddTemperatureInformation(string date, double C, double F, double K) 
+        { 
+            for (int i = 0; i < temperatureInformation.Length; i++)
+            {
+                if (temperatureInformation[i].date == null)
+                {
+                    temperatureInformation[i] = new TemperatureInformation(date, C, F, K);
+                    break;
+                }
+            }
+        }
+
+        public void RemoveTemperatureInformation(string date)
+        {
+            for (int i = 0;i < temperatureInformation.Length; i++)
+            {
+                if (temperatureInformation[i].date == date)
+                {
+                    temperatureInformation[i].date = null;
+                    temperatureInformation[i].C = 0;
+                    temperatureInformation[i].F = 0;
+                    temperatureInformation[i].K = 0;
+                    break;
+                }
+            }
+        }
+
+        public void PrintAllTemperatureInformation()
+        {
+            for (int i = 0; i < temperatureInformation.Length ; i++)
+            {
+                if (temperatureInformation[i].date == null)
+                {
+                    Console.WriteLine("...");
+                }
+                else
+                {
+                    temperatureInformation[i].GetInfo();
+                }
+            }
+        }
+
+        public void GetTempretureByDate(string date)
+        {
+            for (int i = 0;i < temperatureInformation.Length; i++)
+            {
+                if (temperatureInformation[i].date == date)
+                {
+                    temperatureInformation[i].GetInfo();
+                }
+            }
+        }
+
+        public void GetTempretureByPeriod(string start, string end) 
+        {
+            int indexStart = -1, indexEnd = -1;
+            for (int i = 0; i < temperatureInformation.Length; i++)
+            {
+                if (temperatureInformation[i].date == start)
+                {
+                    indexStart = i;
+                }
+                if (temperatureInformation[i].date == end)
+                {
+                    indexEnd = i;
+                    break;
+                }
+            }
+            for (int i = indexStart; i <= indexEnd; i++)
+            {
+                temperatureInformation[i].GetInfo();
+            }
+        }
+    }
+}
---
+++ HomeworkHydrometCentre/Program.cs
@@ -0,0 +1,125 @@
+﻿using System.Reflection.Metadata;
+
+namespace HomeworkHydrometCentre
+{
+    struct TemperatureInformation
+    {
+        public string date { get; set; }
+        public double C { get; set; }
+        public double F { get; set; }
+        public double K { get; set; }
+
+        public TemperatureInformation(string date, double C, double F, double K) 
+        { 
+            this.date = date;
+            this.C = C;
+            this.F = F;
+            this.K = K;
+        }
+
+        public void GetInfo() 
+        {
+            Console.WriteLine($"for the date {date} has been recorded in Celsius: {C}, Forringeit: {F} and Kelvin: {K}.");
+        }
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HydrometCentre hydrometCentre = new HydrometCentre();
+            
+            bool works = true;
+            while (works)
+            {
+                Console.WriteLine("choose what you want");
+                Console.WriteLine("1-if you're an admin");
+                Console.WriteLine("2-see the temperature for a particular day");
+                Console.WriteLine("3-find out the temperature for a certain period");
+                Console.WriteLine("0-exit");
+                string selection = Console.ReadLine();
+
+                switch (selection)
+                {
+                    case "1":
+                        {
+                            Console.WriteLine("1-enter new temperature information");
+                            Console.WriteLine("2-delete date information");
+                            Console.WriteLine("3-display all information ");
+                            Console.WriteLine("0-exit");
+                            string selection2 = Console.ReadLine();
+
+                            switch (selection2)
+                            {
+                                case "1":
+                                    {
+                                        Console.WriteLine("enter the date");
+                                        string date = Console.ReadLine();
+
+                                        Console.WriteLine("enter the temperature in Celsius ");
+                                        double C = Convert.ToDouble(Console.ReadLine());
+
+                                        Console.WriteLine("enter the temperature in Fahrenheit ");
+                                        double F = Convert.ToDouble(Console.ReadLine());
+
+                                        Console.WriteLine("enter the temperature in Kelvin");
+                                        double K = Convert.ToDouble(Console.ReadLine());
+
+                                        hydrometCentre.AddTemperatureInformation(date, C, F, K);
+                                        break;
+                                    }
+                                case "2":
+                                    {
+                                        Console.WriteLine("enter the date you want to delete");
+                                        string date = Console.ReadLine();
+
+                                        hydrometCentre.RemoveTemperatureInformation(date);
+                                        break;
+                                    }
+                                case"3":
+                                    {
+                                        hydrometCentre.PrintAllTemperatureInformation();
+                                        break;
+                                    }
+                                case"0":
+                                    {
+                                        works = false;
+                                        break;
+                                    }
+                                default:
+                                    Console.WriteLine("error try again make a selection (enter 1, 2, 3, 0)");
+                                    break;
+                            }
+                            break;
+                        }
+                    case "2":
+                        {
+                            Console.WriteLine("enter the date for which you want to know the temperature ");
+                            string date = Console.ReadLine();
+
+                            hydrometCentre.GetTempretureByDate(date);
+                            break;
+                        }
+                    case "3":
+                        {
+                            Console.WriteLine("enter from which date you want to know the temperature ");
+                            string start = Console.ReadLine();
+
+                            Console.WriteLine("enter the date for which you want to know the temperature ");
+                            string end = Console.ReadLine();
+
+                            hydrometCentre.GetTempretureByPeriod(start, end);
+                            break;
+                        }
+                    case "0":
+                        works = false;
+                        break;
+                    default:
+                        Console.WriteLine("error try again make a selection (enter 1, 2, 3, 0)");
+                        break;
+                }
+            }
+        }
+    }
+}
